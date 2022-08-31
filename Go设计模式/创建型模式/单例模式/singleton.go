466b81d9521b7095634e4dbf04e2626fdb541cb1
龙军 <570580380@qq.com>
2022-08-31 14:41:49 +0800

    add: 单例模式

--- Go设计模式/创建型模式/单例模式/singleton.go
+++ Go设计模式/创建型模式/单例模式/singleton.go
@@ -8,7 +8,7 @@ type Singleton struct {
 var singleton *Singleton
 var once sync.Once
 
-func GetSingleton() *Singleton {
+func GetInstance() *Singleton {
 	once.Do(func() {
 		singleton = &Singleton{}
 	})
