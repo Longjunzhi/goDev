d43f35fc76855fe0117a808c9efdb129bdef40c3
龙军 <570580380@qq.com>
2022-09-04 15:32:13 +0800

    add: 适配模式

--- Go设计模式/结构性模式/适配器模式/adapter.go
+++ Go设计模式/结构性模式/适配器模式/adapter.go
@@ -1,1 +1,34 @@
 package 适配器模式
+
+type Target interface {
+	Request() string
+}
+
+type Adaptee interface {
+	SpecificRequest() string
+}
+
+func NewAdaptee() Adaptee {
+	return &adapteeImp{}
+}
+
+type adapteeImp struct {
+}
+
+func (a *adapteeImp) SpecificRequest() string {
+	return "adaptee method"
+}
+
+func NewAdapter(adaptee Adaptee) Target {
+	return &adapter{
+		Adaptee: adaptee,
+	}
+}
+
+type adapter struct {
+	Adaptee
+}
+
+func (a *adapter) Request() string {
+	return a.SpecificRequest()
+}
--- Go设计模式/结构性模式/适配器模式/adapter_test.go
+++ Go设计模式/结构性模式/适配器模式/adapter_test.go
@@ -1,1 +1,14 @@
 package 适配器模式
+
+import "testing"
+
+var expect = "adaptee method"
+
+func TestAdapter_Request(t *testing.T) {
+	adaptee := NewAdaptee()
+	target := NewAdapter(adaptee)
+	res := target.Request()
+	if res != expect {
+		t.Fatalf("expect: %s, actual: %s", expect, res)
+	}
+}
