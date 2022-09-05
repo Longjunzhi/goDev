dc493959c7aefbd8ef1f81f58543d2df79eb8274
龙军 <570580380@qq.com>
2022-09-05 10:05:30 +0800

    享元模式

---
+++ Go设计模式/结构性模式/享元模式/flyweight.go
@@ -0,0 +1,57 @@
+package 享元模式
+
+import "fmt"
+
+type ImageFlyweightFactory struct {
+	maps map[string]*ImageFlyweight
+}
+
+var imageFactory *ImageFlyweightFactory
+
+func GetImageFlyweightFactory() *ImageFlyweightFactory {
+	if imageFactory == nil {
+		imageFactory = &ImageFlyweightFactory{
+			maps: make(map[string]*ImageFlyweight),
+		}
+	}
+	return imageFactory
+}
+
+func (f *ImageFlyweightFactory) Get(fileName string) *ImageFlyweight {
+	image := f.maps[fileName]
+	if image == nil {
+		image = NewImageFlyweight(fileName)
+		f.maps[fileName] = image
+	}
+	return image
+}
+
+type ImageFlyweight struct {
+	data string
+}
+
+func NewImageFlyweight(filename string) *ImageFlyweight {
+	data := fmt.Sprintf("image data %s", filename)
+	return &ImageFlyweight{
+		data: data,
+	}
+}
+
+func (i *ImageFlyweight) Data() string {
+	return i.data
+}
+
+type ImageViewer struct {
+	*ImageFlyweight
+}
+
+func NewImageViewer(filename string) *ImageViewer {
+	image := GetImageFlyweightFactory().Get(filename)
+	return &ImageViewer{
+		ImageFlyweight: image,
+	}
+}
+
+func (i *ImageViewer) Display() {
+	fmt.Printf("Display: %s\n", i.Data())
+}
