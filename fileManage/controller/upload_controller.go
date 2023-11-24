5806fac9510487a92a56ee78f513bec680e7d25d
570580380@qq.com <570580380@qq.com>
2023-11-24 09:41:40 +0800

    feat: 上传文件接口

--- fileManage/controller/upload_controller.go
+++ fileManage/controller/upload_controller.go
@@ -3,7 +3,10 @@ package controller
 import (
 	"Img/config"
 	"Img/util"
+	"crypto/md5"
+	"encoding/hex"
 	"github.com/gin-gonic/gin"
+	"io"
 	"net/http"
 )
 
@@ -13,13 +16,21 @@ func Upload(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, err.Error())
 		return
 	}
+	open, err := file.Open()
+	if err != nil {
+		return
+	}
+	hash := md5.New()
+	_, _ = io.Copy(hash, open)
+	md5String := hex.EncodeToString(hash.Sum(nil))
 	err = c.SaveUploadedFile(file, config.AppConf.BasePath+util.GetPathTag()+file.Filename)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, err.Error())
 		return
 	}
-	c.JSON(http.StatusOK, "success")
+	c.JSON(http.StatusOK, md5String)
 }
+
 func UploadMultiple(c *gin.Context) {
 	form, err := c.MultipartForm()
 	if err != nil {
@@ -27,11 +38,19 @@ func UploadMultiple(c *gin.Context) {
 		return
 	}
 	files := form.File["files"]
+	var md5String []string
 	for _, file := range files {
+		open, err := file.Open()
+		if err != nil {
+			return
+		}
+		hash := md5.New()
+		_, _ = io.Copy(hash, open)
+		md5String = append(md5String, hex.EncodeToString(hash.Sum(nil)))
 		if err := c.SaveUploadedFile(file, config.AppConf.BasePath+util.GetPathTag()+file.Filename); err != nil {
 			c.JSON(http.StatusInternalServerError, err.Error())
 			return
 		}
 	}
-	c.JSON(http.StatusOK, "success")
+	c.JSON(http.StatusOK, md5String)
 }
