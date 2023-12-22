6514e335d1a2daaa72690cf39274a0a88877af2f
570580380@qq.com <570580380@qq.com>
2023-12-22 11:10:42 +0800

    feat: build

--- fileManage/constants/app_constant.go
+++ fileManage/constants/app_constant.go
@@ -1,6 +1,6 @@
 package constants
 
 const (
-	// APP_STORAGE_URL = "http://my.pangxuejun.cn/public/"
-	APP_STORAGE_URL = "http://localhost.my.pangxuejun.cn:8080/public/"
+	APP_STORAGE_URL = "https://my-api.pangxuejun.cn/public/"
+	//APP_STORAGE_URL = "http://localhost.my.pangxuejun.cn:8080/public/"
 )
