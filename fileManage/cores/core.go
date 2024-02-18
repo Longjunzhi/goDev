55153eff32144f1253f04a030bfd1ce8b2952efc
570580380@qq.com <570580380@qq.com>
2024-02-18 22:47:52 +0800

    feat: 静态文件

---
+++ fileManage/cores/core.go
@@ -0,0 +1,5 @@
+package cores
+
+func InitCore() {
+
+}
--- fileManage/routes/Route.go
+++ fileManage/routes/Route.go
@@ -15,6 +15,8 @@ func init() {
 	Routes = gin.Default()
 	r := Routes.Use(middlewares.AuthMiddleWare(), middlewares.Cors())
 	r.Static("/public", config.AppConf.StorageConf.Path)
+	r.Static("/static", "static")
+	Routes.StaticFile("/MP_verify_FdfIKB7M8Ge2ZRTG.txt", "static/MP_verify_FdfIKB7M8Ge2ZRTG.txt")
 	r.POST("/api/login", controller.Login)
 	r.POST("/api/media/upload", controller.Upload)
 	r.POST("/api/media/upload/multiple", controller.UploadMultiple)
