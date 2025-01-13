1081d001066b8a719317b234358c13a738eff3d9
570580380@qq.com <570580380@qq.com>
2025-01-13 17:51:30 +0800

    feat: 阿里云更新ip

--- aliyunDNS云解析/main.go
+++ aliyunDNS云解析/main.go
@@ -1,17 +1,16 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	alidns20150109 "github.com/alibabacloud-go/alidns-20150109/v4/client"
 	openapi "github.com/alibabacloud-go/darabonba-openapi/v2/client"
 	util "github.com/alibabacloud-go/tea-utils/v2/service"
 	"github.com/alibabacloud-go/tea/tea"
 	"github.com/spf13/viper"
-	"net"
 	"os"
 	"path/filepath"
 	"strings"
-	"time"
 )
 
 /*
@@ -28,58 +27,51 @@ type AccessKey struct {
 
 type AliyunCloudResolution struct {
 	AccessKey AccessKey `json:"access_key"`
-	Hosts     []string  `json:"hosts"`
 }
 
 var MyAliyunCloudResolution AliyunCloudResolution
 
 func main() {
 	GetConfig()
-	ip2409IPv6s := get2409IPv6s()
-	if len(ip2409IPv6s) == 0 {
-		_ = fmt.Errorf("没有获取到移动2409开头的IPv6地址")
+
+	// 定义命令行参数
+	var host string
+	var ip string
+
+	flag.StringVar(&host, "host", "", "The full domain name (e.g., dfs.sd.pangxuejun.cn).")
+	flag.StringVar(&ip, "ip", "", "The IP addresses to update, separated by commas (e.g., 234.343.34,123.45.67.89).")
+
+	// 解析命令行参数
+	flag.Parse()
+
+	// 检查必需的参数是否已提供
+	if host == "" || ip == "" {
+		fmt.Println("Usage: go run main.go -host <host> -type <type> -ip <ip>")
+		os.Exit(1)
+	}
+	ipType := "A"
+	// 如果IP字符串ipv6 则ipType等于"AAAA"
+	if strings.Contains(ip, ":") {
+		ipType = "AAAA"
+	}
+	// 提取一级域名前面的部分
+	parts := strings.Split(host, ".")
+	if len(parts) < 3 {
+		fmt.Printf("Invalid domain: %s\n", host)
 		return
 	}
-	fmt.Printf("IPv6地址2409开头: %v", ip2409IPv6s)
-	hosts := MyAliyunCloudResolution.Hosts
-	if hosts == nil || len(hosts) == 0 {
-		_ = fmt.Errorf("没有配置域名")
-		return
-	}
-	for _, host := range hosts {
-		err := UpdateIp(host[:strings.Index(host, ".")], "AAAA", ip2409IPv6s, host)
-		time.Sleep(time.Duration(60) * time.Second)
-		if err != nil {
-			continue
-		}
-	}
-
-}
-
-func get2409IPv6s() (ips []string) {
-	addrs, err := net.InterfaceAddrs()
+	subDomain := strings.Join(parts[:2], ".")
+	fmt.Println("subDomain: %s, ipType: %s, ip: %s, host: %s", subDomain, ipType, []string{ip}, host)
+	err := UpdateIp(subDomain, ipType, ip, host)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	for _, address := range addrs {
-		if ipNet, ok := address.(*net.IPNet); ok && !ipNet.IP.IsLoopback() {
-			if ipNet.IP.To4() != nil {
-			} else if ipNet.IP.To16() != nil {
-				ipV6 := ipNet.IP.To16().String()
-				if strings.HasPrefix(ipV6, "2409") {
-					ips = append(ips, ipNet.IP.To16().String())
-				}
-			}
-		}
-	}
-	return
 }
 
-func UpdateIp(RR string, Type string, ips []string, subDomain string) (_err error) {
+func UpdateIp(RR string, Type string, ip string, subDomain string) (_err error) {
 	// 请确保代码运行环境设置了环境变量 ALIBABA_CLOUD_ACCESS_KEY_ID 和 ALIBABA_CLOUD_ACCESS_KEY_SECRET。
 	// 工程代码泄露可能会导致 AccessKey 泄露，并威胁账号下所有资源的安全性。以下代码示例使用环境变量获取 AccessKey 的方式进行调用，仅供参考，建议使用更安全的 STS 方式，更多鉴权访问方式请参见：https://help.aliyun.com/document_detail/378661.html
-
 	myAccessKey := MyAliyunCloudResolution.AccessKey.AccessKeyId
 	myAccessKeySecret := MyAliyunCloudResolution.AccessKey.AccessKeySecret
 
@@ -87,7 +79,6 @@ func UpdateIp(RR string, Type string, ips []string, subDomain string) (_err erro
 	if _err != nil {
 		return _err
 	}
-
 	// 获取解析记录详情
 	describeSubDomainRecordsRequest := &alidns20150109.DescribeSubDomainRecordsRequest{
 		SubDomain: tea.String(subDomain),
@@ -97,22 +88,24 @@ func UpdateIp(RR string, Type string, ips []string, subDomain string) (_err erro
 	if err != nil {
 		return err
 	}
+	if len(options.Body.DomainRecords.Record) == 0 {
+		fmt.Println("未查询到域名解析记录")
+		return
+	}
 	record := options.Body.DomainRecords.Record[0]
 	recordId := record.RecordId
 	value := record.Value
-	//fmt.Printf("获取域名recordId:%v，value: %v", *recordId, *value)
-	for _, v := range ips {
-		if v == *value {
-			fmt.Printf("域名解析检测到相同 v:%v，value: %v", v, *value)
-			return
-		}
+	fmt.Printf("获取域名recordId:%v，value: %v", *recordId, *value)
+	if ip == *value {
+		fmt.Printf("域名解析检测到相同 v:%v，value: %v", ip, *value)
+		return
 	}
-	fmt.Printf("域名解析更新，value: %v", ips[0])
+	fmt.Printf("域名解析更新，value: %v", ip)
 	updateDomainRecordRequest := &alidns20150109.UpdateDomainRecordRequest{
 		RecordId: tea.String(*recordId),
 		RR:       tea.String(RR),
 		Type:     tea.String(Type),
-		Value:    tea.String(ips[0]),
+		Value:    tea.String(ip),
 	}
 	updateRuntime := &util.RuntimeOptions{}
 	_, _err = client.UpdateDomainRecordWithOptions(updateDomainRecordRequest, updateRuntime)
@@ -122,13 +115,6 @@ func UpdateIp(RR string, Type string, ips []string, subDomain string) (_err erro
 	return _err
 }
 
-/**
- * 使用AK&SK初始化账号Client
- * @param accessKeyId
- * @param accessKeySecret
- * @return Client
- * @throws Exception
- */
 func CreateClient(accessKeyId *string, accessKeySecret *string) (_result *alidns20150109.Client, _err error) {
 	config := &openapi.Config{
 		// 必填，您的 AccessKey ID
@@ -143,7 +129,7 @@ func CreateClient(accessKeyId *string, accessKeySecret *string) (_result *alidns
 	return _result, _err
 }
 
-func GetConfig() (accessKeyId string, accessKeySecret string) {
+func GetConfig() {
 	path, err := os.Executable()
 	if err != nil {
 		panic(err)
